675d87840e415a06953c9ef3c2ab1862a8c03675
Berk Çolak <colak@intranet.com.tr>
2021-11-15 07:31:06 +0300

    - External libraries added.

--- TestProject.DesktopApp/Fragments/MainJs/app.js
+++ TestProject.DesktopApp/Fragments/MainJs/app.js
@@ -1,39 +1,42 @@
-const { app, BrowserWindow, ipcMain, nativeTheme } = require('electron')
+const { app, screen, Menu, BrowserWindow, ipcMain, nativeTheme } = require('electron')
 const path = require('path')
 
 
 function createWindow() {
+    const primaryDisplay = screen.getPrimaryDisplay()
+    const { width, height } = primaryDisplay.workAreaSize
     const win = new BrowserWindow({
-        width: 800,
-        height: 600,
+        width: width ,
+        height: height,
         webPreferences: {
             preload: path.join(__dirname, 'preload.js')
         }
     })
-    win.loadFile(path.join(__dirname, '../../View/Index/index.html'))
+    win.loadFile(path.join(__dirname, '../../View/Login/Login.html'))
     nativeTheme.themeSource = 'dark'
-    const INCREMENT = 0.03
-    const INTERVAL_DELAY = 100 // ms
+    //const INCREMENT = 0.03
+    //const INTERVAL_DELAY = 100 // ms
+    //let progressInterval
+    //let c = 0
+    //progressInterval = setInterval(() => {
+    //    // update progress bar to next value
+    //    // values between 0 and 1 will show progress, >1 will show indeterminate or stick at 100%
+    //    win.setProgressBar(c)
 
-    let c = 0
-    progressInterval = setInterval(() => {
-        // update progress bar to next value
-        // values between 0 and 1 will show progress, >1 will show indeterminate or stick at 100%
-        win.setProgressBar(c)
-
-        // increment or reset progress bar
-        if (c < 2) {
-            c += INCREMENT
-        } else {
-            c = (-INCREMENT * 5) // reset to a bit less than 0 to show reset state
-        }
-    }, INTERVAL_DELAY)
+    //    // increment or reset progress bar
+    //    if (c < 2) {
+    //        c += INCREMENT
+    //    } else {
+    //        c = (-INCREMENT * 5) // reset to a bit less than 0 to show reset state
+    //    }
+    //}, INTERVAL_DELAY)
+    win.setMenu(null)
+    
 }
-let progressInterval
 
 app.whenReady().then(() => {
     createWindow()
-
+    
     app.on('activate', () => {
         if (BrowserWindow.getAllWindows().length === 0) {
             createWindow()
