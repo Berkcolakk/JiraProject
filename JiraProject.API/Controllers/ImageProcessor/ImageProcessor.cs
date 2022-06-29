2e53412e13788ce6f4f7e2df40ebaca1846fee64
Berk Çolak <colak@intranet.com.tr>
2022-06-29 19:36:52 +0300

    - ImageProcessor controller added.

---
+++ JiraProject.API/Controllers/ImageProcessor/ImageProcessor.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Drawing;
+using System.Threading.Tasks;
+
+namespace JiraProject.API.Controllers.ImageProccessor
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImageProcessor : ControllerBase
+    {
+        const int size = 150;
+        const int quality = 75;
+        public async Task<IActionResult> LoginWithCamera()
+        {
+            return Ok();
+        }
+        public async Task<IActionResult> SetMyProfile()
+        {
+            return Ok();
+        }
+    }
+}
